8b8b1d0203c63994e34cddc6d64658ed440694e1
Dara Hak <dara.hak@gmail.com>
2020-12-15 00:33:02 +0100

    Day 14 (part 2)

--- day14/index.js
+++ day14/index.js
@@ -20,7 +20,9 @@ module.exports = {
           }
         }
 
-        memory.set(index, Number.parseInt(bin.join(''), 2));
+        const alteredBin = bin.join('');
+
+        memory.set(index, Number.parseInt(alteredBin, 2));
       }
     });
 
@@ -36,6 +38,40 @@ module.exports = {
   /** @argument input {string} */
   second(input) {
     const lines = input.trim().split('\n');
+
+    let mask = '';
+    /** @type {Map<number, number>} */
+    let memory = new Map();
+
+    lines.forEach((line) => {
+      if (line.startsWith('mask')) {
+        mask = getMask(line);
+      } else if (line.startsWith('mem')) {
+        const { index, value } = getMem(line);
+        const bin = toBinaryString(index).split('');
+
+        for (let i = 0; i < bin.length; ++i) {
+          if (mask[i] !== '0') {
+            bin[i] = mask[i];
+          }
+        }
+
+        const alteredBin = bin.join('');
+        const addresses = computeAddresses(alteredBin, mask);
+
+        addresses.forEach((address) => {
+          memory.set(address, value);
+        });
+      }
+    });
+
+    let sum = 0;
+
+    memory.forEach((value) => {
+      sum += value;
+    });
+
+    return sum;
   },
 };
 
@@ -69,4 +105,38 @@ function getMem(line) {
  */
 function toBinaryString(value) {
   return value.toString(2).padStart(36, '0');
+}
+
+/**
+ * @param {string} value
+ * @returns {Array<number>}
+ */
+function computeAddresses(value) {
+  let xPositions = [];
+
+  for (let i = 0; i < value.length; ++i) {
+    if (value[i] === 'X') {
+      xPositions.push(i);
+    }
+  }
+
+  const max = Math.pow(2, xPositions.length);
+
+  let addresses = [];
+
+  for (let x = 0; x < max; ++x) {
+    const bin = x.toString(2).padStart(xPositions.length, '0');
+
+    let valueArray = value.split('');
+
+    for (let i = 0; i < bin.length; ++i) {
+      valueArray[xPositions[i]] = bin[i];
+    }
+
+    const alteredValue = valueArray.join('');
+
+    addresses.push(Number.parseInt(alteredValue, 2));
+  }
+
+  return addresses;
 }
